63ab63a8a1731aad2cfbe064ce21109301dad699
KISHORE KUMARESAN <97290646+KISHOREKUMARESAN06@users.noreply.github.com>
2024-07-31 07:54:49 +0530

    Update login_page.py

--- pagepack/login_page.py
+++ pagepack/login_page.py
@@ -11,7 +11,6 @@ from utilpack.util_exl import XLU
 class Login_cls:
 
     def __init__(self, driver):
-        super().__int__(driver)
         self.driver = driver
 
     # method of login with username and password.
